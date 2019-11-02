06e9ac66c67344eae12731d30769da0d7c238966
calbers1 <calbers1@harding.edu>
2019-11-01 22:17:28 -0500

    Finally fixed huffman table

--- Huff.cpp
+++ Huff.cpp
@@ -8,7 +8,6 @@
 #include <bitset>
 #include <sstream>
 using namespace std;
-const int BITNUM = 20;
 const int CHARNUM = 256;
 const int MAX_ENTRIES = 513;
 //makes a node to hold frequency, glyph, and l/r pointers for the huffman table or tree
@@ -42,10 +41,7 @@ void main() {
 	sortedTable = deleteEmptyGlyphs(frequencyTable);
 	createTable(sortedTable, huffTable);
 
-	for (int i = 0; i < (frequencyTable.size() * 2) - 1; i++) {
-		cout << i << ") " << huffTable[i].glyph << " : " << huffTable[i].frequency;
-		cout << ", " << huffTable[i].left << " < --- > " << huffTable[i].right << endl;
-	}
+	
 
 	fileIn.close();
 	fileOut.close();
@@ -65,6 +61,7 @@ glyphTable format: glyphTable[frequency] = glyph
 void createTable(multimap<int, unsigned char> &glyphTable, huffNode* huffTable) {
 	int m, h, f, n;
 	huffNode temp;
+	huffNode testTable[MAX_ENTRIES];
 	int replacement;
 	f = glyphTable.size();
 	h = glyphTable.size() - 1;
@@ -72,47 +69,49 @@ void createTable(multimap<int, unsigned char> &glyphTable, huffNode* huffTable) 
 	//Set the initial values for the huffman table (the glyphs from the frequency table)
 	int j = 0;
 	for (auto i : glyphTable) {
-		huffTable[j].frequency = i.first;
-		huffTable[j].glyph = i.second;
+		testTable[j].frequency = i.first;
+		testTable[j].glyph = i.second;
 		j++;
 	}
 	//for every glyph in the table:
 	for (int i = 0; i < (glyphTable.size() - 1); i++) {
 		//mark(m) lower of relative slots 2 and 3 ([1] and [2])
-		m = huffTable[1].frequency <= huffTable[2].frequency ? 1 : 2;
-
+		if (h > 1) {
+			m = testTable[1].frequency <= testTable[2].frequency ? 1 : 2;
+		}
+		else { m = 1; }
 		//move (m) to the next free slot
-		huffTable[f] = huffTable[m];
+		testTable[f] = testTable[m];
 
 		//move h to m
-		huffTable[m] = huffTable[h];
+		testTable[m] = testTable[h];
 
 		//check for reheap of m slot
 		replacement = m;
 		//if left is valid
 		if ((m * 2) + 1 < h) {
 			//if left < m slot, set left to replacement
-			if (huffTable[m].frequency > huffTable[(m * 2) + 1].frequency) {
+			if (testTable[m].frequency > testTable[(m * 2) + 1].frequency) {
 				replacement = (m * 2) + 1;
 
 			}
 			//if right is valid and less than replacement, set right to replacement
-			if ((m * 2) + 2 < h &&  huffTable[(m * 2) + 2].frequency < huffTable[replacement].frequency) {
+			if ((m * 2) + 2 < h &&  testTable[(m * 2) + 2].frequency < testTable[replacement].frequency) {
 				replacement = (m * 2) + 2;
 			}
 		}
 		//replace the m slot with replacement (if no replacement, m stays the same)
-		temp = huffTable[m];
-		huffTable[m] = huffTable[replacement];
-		huffTable[replacement] = temp;
+		temp = testTable[m];
+		testTable[m] = testTable[replacement];
+		testTable[replacement] = temp;
 		//move slot 0 to (h)
-		huffTable[h] = huffTable[0];
+		testTable[h] = testTable[0];
 
 		//create freq node at 0
-		huffTable[0].frequency = huffTable[h].frequency + huffTable[f].frequency;
-		huffTable[0].glyph = -1;
-		huffTable[0].left = h;
-		huffTable[0].right = f;
+		testTable[0].frequency = testTable[h].frequency + testTable[f].frequency;
+		testTable[0].glyph = -1;
+		testTable[0].left = h;
+		testTable[0].right = f;
 
 		//check for reheap of all slots
 		for (n = 0; n < h; n++) {
@@ -121,24 +120,28 @@ void createTable(multimap<int, unsigned char> &glyphTable, huffNode* huffTable) 
 			//if left is valid
 			if ((n * 2) + 1 < h) {
 				//if left < n slot, set left to replacement
-				if (huffTable[n].frequency > huffTable[(n * 2) + 1].frequency) {
+				if (testTable[n].frequency > testTable[(n * 2) + 1].frequency) {
 					replacement = (n * 2) + 1;
 
 				}
 				//if right is valid and less than replacement, set right to replacement
-				if ((n * 2) + 2 < h &&  huffTable[(n * 2) + 2].frequency < huffTable[replacement].frequency) {
+				if ((n * 2) + 2 < h &&  testTable[(n * 2) + 2].frequency < testTable[replacement].frequency) {
 					replacement = (n * 2) + 2;
 				}
 			}
 			//replace the n slot with replacement (if no replacement, n stays the same)
-			temp = huffTable[n];
-			huffTable[n] = huffTable[replacement];
-			huffTable[replacement] = temp;
+			temp = testTable[n];
+			testTable[n] = testTable[replacement];
+			testTable[replacement] = temp;
 		}
 
 		h--;
 		f++;
 
+	}
+	for (int i = 0; i < (glyphTable.size() * 2) - 1; i++) {
+		cout << i << ") " << testTable[i].glyph << " : " << testTable[i].frequency;
+		cout << ", " << testTable[i].left << " < --- > " << testTable[i].right << endl;
 	}
 }
 
